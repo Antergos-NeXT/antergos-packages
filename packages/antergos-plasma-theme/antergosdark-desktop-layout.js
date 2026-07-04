fba61a93b00b85c48fb743d9eb7453d449c75c4e
Celestia Ludenberg <ash8820@proton.me>
2026-07-04 22:23:49 +0200

    fix: set wallpaper image directly in layout JS script
    lookandfeeltool's defaults [Wallpaper] section doesn't reliably set
    the wallpaper image path. Set it explicitly in the layout.js so it
    always applies.

--- packages/antergos-plasma-theme/antergosdark-desktop-layout.js
+++ packages/antergos-plasma-theme/antergosdark-desktop-layout.js
@@ -3,4 +3,7 @@ loadTemplate("org.kde.plasma.desktop.defaultPanel")
 var desktopsArray = desktopsForActivity(currentActivity());
 for( var j = 0; j < desktopsArray.length; j++) {
     desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+    desktopsArray[j].currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
+    desktopsArray[j].writeConfig('Image', 'file:///usr/share/backgrounds/antergos/antergos-wallpaper.png');
+    desktopsArray[j].writeConfig('FillMode', 2);
 }
